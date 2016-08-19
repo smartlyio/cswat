17603868b3e5780c8bfaea7d76da2646c4770e18
Arash Badie Modiri <arashbm@gmail.com>
2016-08-19 10:35:54 +0300

    using minitest::reporters

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,10 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'cswat'
 
 require 'minitest/autorun'
+
+require "minitest/reporters"
+Minitest::Reporters.use!
+
 require 'test/unit/assertions'
 
 include Test::Unit::Assertions
