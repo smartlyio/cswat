b381fbfcc3632229b3883f69492f4a86582c6567
Arash Badie Modiri <arashbm@gmail.com>
2016-08-29 10:07:10 +0300

    change bench_range to something more sensible

--- test/test_benchmarks.rb
+++ test/test_benchmarks.rb
@@ -1,8 +1,11 @@
 require "test_helper"
 
 class TestCSWat < Minitest::Benchmark
-  def bench_default_performance
-    assert_performance_linear 0.999 do |n| # n is a range value
+  def bench_range
+    [100, 1_000, 10_000, 100_000, 1_000_000]
+  end
+  def bench_default_options_performance
+    assert_performance_linear 0.999 do |n|
       CSWat.parse("1,2.1,\"Dwayne \"\"The Rock\"\" Johnson\",-1\n"*n)
     end
   end
